951a0674cadb71909d8a11950bbb4578e07c41d3
Lars Wallenborn <lars@wallenborn.net>
2017-10-09 02:26:31 +0200

    refactoring, combines processors

--- extractor/file.py
+++ extractor/file.py
@@ -1,14 +1,24 @@
 from lib import entropy
 from .base import BaseExtractor
+from datetime import datetime
+import hashlib
 
 
 class File(BaseExtractor):
-    def __init__(self, data, pe, regex_factory):
+    def __init__(self, data, pe):
         self.data = data
         self.pe = pe
 
     def extract(self, sample):
+        sample.hash_md5 = hashlib.md5(self.data).hexdigest()
+        sample.hash_sha1 = hashlib.sha1(self.data).hexdigest()
+        sample.hash_sha256 = hashlib.sha256(self.data).hexdigest()
+        sample.ssdeep = ssdeep.hash(self.data)
+
         sample.size = len(self.data)
         sample.entropy = entropy(self.data)
         sample.entry_point = self.pe.OPTIONAL_HEADER.AddressOfEntryPoint
         sample.first_kb = [c for c in self.data[:1024]]
+
+        if hasattr(self.pe, 'FILE_HEADER'):
+            sample.build_timestamp = datetime.utcfromtimestamp(self.pe.FILE_HEADER.TimeDateStamp)
