b2aec8039867803a692d2a227e7e5811c5e91dbb
Lars Wallenborn <lars@wallenborn.net>
2017-10-15 02:29:51 +0200

    new way of pdb handling

--- extractor/pdb.py
+++ extractor/pdb.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from .base import BaseExtractor
 from lib.regex import null_terminate_and_decode_utf8
+from lib.data import SampleDebugDirectory
 
 
 class Pdb(BaseExtractor):
@@ -11,54 +12,29 @@ class Pdb(BaseExtractor):
         if not hasattr(self.pe, 'DIRECTORY_ENTRY_DEBUG'):
             return
 
-        debug_timestamps = []
-        pdb_paths = []
-        pdb_ages = []
-        pdb_signatures = []
-        pdb_guids = []
-        sample.debug_directory_count = len(self.pe.DIRECTORY_ENTRY_DEBUG)
-
+        directories = []
         for debug_data in self.pe.DIRECTORY_ENTRY_DEBUG:
             if not hasattr(debug_data, 'entry'):
                 continue
+            directory = SampleDebugDirectory()
             if hasattr(debug_data.entry, 'CvSignature'):
-                pdb_signatures.append(debug_data.entry.CvSignature)
-            guid = '-'.join([
+                directory.signature = debug_data.entry.CvSignature
+            sig_data = [
                 '%x' % getattr(debug_data.entry, e)
                 for e in dir(debug_data.entry)
                 if e.startswith('Signature_Data')
-            ])
-            if guid: pdb_guids.append(guid)
+            ]
+            if sig_data: directory.guid = '-'.join(sig_data)
 
             if hasattr(debug_data.entry, 'Age'):
-                pdb_ages.append(debug_data.entry.Age)
+                directory.age = debug_data.entry.Age
             if debug_data.struct.TimeDateStamp:
-                debug_timestamps.append(debug_data.struct.TimeDateStamp)
+                directory.timestamp = datetime.utcfromtimestamp(debug_data.struct.TimeDateStamp)
             if hasattr(debug_data.entry, 'PdbFileName'):
-                pdb_paths.append(null_terminate_and_decode_utf8(debug_data.entry.PdbFileName))
+                directory.path = null_terminate_and_decode_utf8(debug_data.entry.PdbFileName)
 
-        debug_timestamps = list(set(debug_timestamps))
-        if len(debug_timestamps) > 1:
-            raise Exception('Found %i debug timestamps.' % len(debug_timestamps))
+            if directory.signature or directory.guid or directory.age or directory.timestamp or directory.path:
+                directories.append(directory)
 
-        pdb_ages = list(set(pdb_ages))
-        if len(pdb_paths) > 1:
-            raise Exception('Found %i debug ages.' % len(pdb_ages))
-
-        pdb_paths = list(set(pdb_paths))
-        if len(pdb_paths) > 1:
-            raise Exception('Found %i debug paths.' % len(pdb_paths))
-
-        pdb_signatures = list(set(pdb_signatures))
-        if len(pdb_signatures) > 1:
-            raise Exception('Found %i pdb signatures.' % len(pdb_signatures))
-
-        pdb_guids = list(set(pdb_guids))
-        if len(pdb_guids) > 1:
-            raise Exception('Found %i pdb GUIDs.' % len(pdb_guids))
-
-        if debug_timestamps: sample.debug_timestamp = datetime.utcfromtimestamp(debug_timestamps[0])
-        if pdb_ages: sample.pdb_age = pdb_ages[0]
-        if pdb_paths: sample.pdb_path = pdb_paths[0]
-        if pdb_signatures: sample.pdb_signature = pdb_signatures[0]
-        if pdb_guids: sample.pdb_guid = pdb_guids[0]
+        if directories:
+            sample.debug_directories = directories
