efebae0fc8cadac59ea0c3f1573e0d7a1045e611
Lars Wallenborn <lars@wallenborn.net>
2017-10-15 18:28:05 +0200

    performance measurement prototype

--- extractor/file.py
+++ extractor/file.py
@@ -1,4 +1,5 @@
 from lib.regex import entropy
+from lib.performance import SubTimer
 from .base import BaseExtractor
 from datetime import datetime
 import hashlib
@@ -7,23 +8,29 @@ from magic import magic
 
 
 class File(BaseExtractor):
-    def __init__(self, data, pe, peyd):
+    def __init__(self, data, pe, peyd, timer):
         self.data = data
         self.pe = pe
         self.peyd = peyd
+        self.timer = timer
 
     def extract(self, sample):
+        timer = SubTimer(self.timer)
+
+        timer.mark('hash')
         sample.hash_md5 = hashlib.md5(self.data).hexdigest()
         sample.hash_sha1 = hashlib.sha1(self.data).hexdigest()
         sample.hash_sha256 = hashlib.sha256(self.data).hexdigest()
         sample.ssdeep = ssdeep.hash(self.data)
-        sample.magic = magic.from_buffer(self.data[:1024])
 
+        timer.mark('meta')
+        sample.magic = magic.from_buffer(self.data[:1024])
         sample.size = len(self.data)
         sample.entropy = entropy(self.data)
         sample.entry_point = self.pe.OPTIONAL_HEADER.AddressOfEntryPoint
         sample.first_kb = [c for c in self.data[:1024]]
-        sample.peyd = [m for m in self.peyd.all_matches(self.pe)]
-
         if hasattr(self.pe, 'FILE_HEADER'):
             sample.build_timestamp = datetime.utcfromtimestamp(self.pe.FILE_HEADER.TimeDateStamp)
+
+        timer.mark('peyd')
+        sample.peyd = [m for m in self.peyd.all_matches(self.pe)]
--- process.py
+++ process.py
@@ -10,23 +10,26 @@ from pprint import pprint
 import pefile
 
 from graphite import Graphite
+from lib.performance import PerformanceTimer, NullTimer, SubTimer
 from lib.regex import RegexFactory
 from lib.data import Sample, JsonFactory
 from peyd.peyd import PEiDDataBase
 
 logging.basicConfig(format='%(asctime)s %(message)s')
 logger = logging.getLogger('KurasutaWorker')
-
 script_folder = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(script_folder)
 
 parser = argparse.ArgumentParser(description='extracts metadata from PE file (part of the Kurasuta project)')
 parser.add_argument('--debug', action='store_true', help='Show debugging information')
 parser.add_argument('--pretty', action='store_true', help='Uses pretty print')
+parser.add_argument('--performance', action='store_true', help='Measure performance and output report')
 parser.add_argument('--filter', help='Specify pattern that output fields must match')
 parser.add_argument('file_name', metavar='FILENAME', help='file to process')
 args = parser.parse_args()
 
+timer = PerformanceTimer(logger) if args.performance else NullTimer()
+timer.mark('start')
 logger.setLevel(logging.DEBUG if args.debug else logging.WARNING)
 
 if 'RAVEN_CLIENT_STRING' in os.environ:
@@ -43,12 +46,14 @@ else:
     graphite = None
     logger.warning('Environment variable GRAPHITE_SERVER does not exist. No logging to Graphite is performed.')
 
+regex_factory = RegexFactory()
+timer.mark('read_file')
 file_data = open(args.file_name, 'rb').read()
+timer.mark('parse_pe')
 pe = pefile.PE(data=file_data)
+timer.mark('init_peyd')
 peyd = PEiDDataBase()
 peyd.readfile(os.path.join(os.path.dirname(__file__), 'peyd', 'peyd.txt'))
-
-regex_factory = RegexFactory()
 
 
 def get_extractors():
@@ -68,6 +73,12 @@ def get_extractors():
                 continue
             signature = inspect.signature(class_object.__init__)
 
+            if len(class_object.__bases__) != 1:
+                continue
+            base_class = class_object.__bases__[0]
+            if base_class.__name__ != 'BaseExtractor':
+                continue
+
             kwargs = {}
             for parameter in signature.parameters:
                 if parameter == 'self': continue
@@ -76,21 +87,18 @@ def get_extractors():
                 if parameter == 'logger': kwargs['logger'] = logger
                 if parameter == 'regex_factory': kwargs['regex_factory'] = regex_factory
                 if parameter == 'peyd': kwargs['peyd'] = peyd
-
-            if len(class_object.__bases__) != 1:
-                continue
-            base_class = class_object.__bases__[0]
-            if base_class.__name__ != 'BaseExtractor':
-                continue
+                if parameter == 'timer': kwargs['timer'] = timer
 
             extractors[name] = class_object(**kwargs)
     return extractors
 
 
+timer.mark('read_extractors')
 sample = Sample()
 extractors = get_extractors().values()
 logger.debug('Enabled Extractors: %s' % extractors)
 for extractor in extractors:
+    timer.mark('extractor_%s' % extractor.__class__.__name__)
     try:
         extractor.extract(sample)
     except Exception as e:
@@ -98,8 +106,21 @@ for extractor in extractors:
         if sentry:
             sentry.captureException()
 
+timer.mark('output')
 out = JsonFactory(args.filter).from_sample(sample)
 if args.pretty:
     pprint(out)
 else:
     print(json.dumps(out))
+timer.mark('end')
+
+if args.performance:
+    from terminaltables import AsciiTable as TerminalTable
+
+    table_data = [['Operation', 'perf_count', 'process_time']]
+    for i in range(1, len(timer) - 2):
+        perf_count_diff = timer[i + 1].perf_count - timer[i].perf_count
+        process_time_diff = timer[i + 1].process_time - timer[i].process_time
+        table_data.append([timer[i].caption, '%.7f' % perf_count_diff, '%.7f' % process_time_diff])
+
+    print(TerminalTable(table_data).table)
